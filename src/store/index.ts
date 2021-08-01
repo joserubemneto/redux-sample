2d137b34839e799ebdd2a32cbc6162cd5aa1ce86
Jose Rubem <joserubemn31@gmail.com>
2021-08-01 16:30:33 +0100

    added sample

---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from "redux";
+import { ICartState } from "./modules/cart/types";
+import rootReducer from "./modules/rootReducer";
+import rootSaga from "./modules/rootSaga";
+
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+export interface IState {
+  cart: ICartState;
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
